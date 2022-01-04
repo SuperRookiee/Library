07f054e71218a61381e76394eaa8600142008cf2
SuperRookiee <hyunuk0417@gmail.com>
2022-01-04 22:09:57 +0900

    BookController + searchTitle추가 -  searchIsbn 삭제

--- src/main/java/org/book/controller/BookController.java
+++ src/main/java/org/book/controller/BookController.java
@@ -1,26 +1,43 @@
 package org.book.controller;
 
-import org.book.domain.BookDTO;
-import org.book.service.BookService;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+
 @Controller
 @Log4j
 @RequestMapping("/book/*")
 @AllArgsConstructor
 public class BookController {
-	@GetMapping("/searchIsbn")
-	@ResponseBody
-	public BookDTO searchIsbn(@RequestParam("isbn") String isbn) {
-//		BookDTO isbnSearch = BookService.searchIsbn(isbn);
-//		return isbnSearch;
-		return null;
+
+	@GetMapping("/searchTitle")
+	public String searchTitle(String key) {
+		Document doc;
+		String tit = "";
+		String url = "https://www.nl.go.kr/NL/search/openApi/search.do?key=11d02e96170b0f22dcabf71d0443089a7b104ebc9ab04749291c3e659cb3111f&kwd=";
+
+		try {
+			doc = Jsoup.connect(url + key).get();
+			Elements title = doc.getElementsByTag("title_info");
+			if (title.size() > 0) {
+				for (int i = 0; i < title.size(); i++) {
+					tit = title.get(i).text();
+					log.info(tit);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return tit;
 	}
 
 }
